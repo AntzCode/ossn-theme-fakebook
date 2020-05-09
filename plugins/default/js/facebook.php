e528bfccb806e9da50113438a6bfa665e7605d49
Happy Hive Worker <hivelife@antzcode.com>
2020-05-09 20:46:49 +0200

    Links should be clickable in Private Messages #6

--- plugins/default/js/facebook.php
+++ plugins/default/js/facebook.php
@@ -42,8 +42,82 @@ $(document).ready(function() {
 		}
 	    }
 	    
+	});
+	
+	
+	$('.ossn-widget.messages-with').delegate('.message-inner', 'DOMSubtreeModified', function(){
+	    
+	    if($(this).data('working')>0){
+		// do not trigger recursive events!
+		return;
+	    }else{
+		$(this).data('working', 1);
+	    }
+	    
+	    $('> .row .text', this).each(function(){
+		
+		var tokens = [];
+		
+		var hashString = function(text, options){
+		    var hash = 0;
+		    if(text.length === 0){
+		      return hash;
+		    }
+		    for(var i = 0; i < text.length; i++){
+		      var char = text.charCodeAt(i);
+		      hash = ((hash<<5)-hash)+char;
+		      hash = hash & hash; // Convert to 32bit integer
+		    }
+		    if(hash < 0){
+		      // convert to positive integer
+		      hash = 0-hash;
+		    }
+		    var prefix = options.prefix || 'STHASH';
+		    var suffix = options.suffix || 'HSAHTS';
+		    return prefix+hash+suffix;
+		};
+		
+		var tokenizeString = function(text, options){
+		    options = options || {};
+		    var token = hashString(text, options);
+		    tokens[token] = text;
+		    return token;
+		};
+  
+		var detokenizeString = function(token){
+		  return tokens[token];
+		};
+  
+		var processedText = $(this).html();
+		
+		// we do not nest links inside existing links
+		processedText = processedText.replace(/(<a.*?<\/a>)/g, function(match){
+		  var token = tokenizeString(match, {prefix:'STEXEMPT',suffix:'TPMEXETS'});
+		  return token;
+		});
+		
+		// convert non-linked urls into links
+		processedText = processedText.replace(/http[s]?:\/\/[^\]][^\s<]*/g, function(match){
+		  return '<a href="'+match+'" target="_blank">'+match+'</a>';
+		});
+		
+		// restore existing links
+		processedText = processedText.replace(/(STEXEMPT[0-9]*TPMEXETS)/g, function(match){
+		  return detokenizeString(match);
+		});
+		
+		$(this).html(processedText);
+		
+	    });
+	    
+	    // reset the flag - permit future processes to resume
+	    $(this).data('working', 0);
 	    
 	});
+	
+	$('.ossn-widget.messages-with .message-inner').trigger('DOMSubtreeModified');
+	
+	
     
 	$(document).on('click', '#sidebar-toggle', function() {
 		var $toggle = $(this).attr('data-toggle');
